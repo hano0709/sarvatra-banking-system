a5682ca067832372dd9ef6c6606fb31331728f5c
Hano Varghese <hanojoby@gmail.com>
2026-01-12 12:58:27 +0530

    transfer detail init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Home } from './home/home';
+import { TransferDetails } from './components/transfer-details/transfer-details';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: Login},
-    { path: 'home', component: Home }
+    { path: 'home', component: Home },
+    {
+        path: 'components',
+        component: Home,
+        children: [
+            {path: 'transfer-details', component: TransferDetails}
+        ]
+    }
 ];
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { Sidebar } from '../components/sidebar/sidebar';
 import { Header } from '../components/header/header';
+import { Router, RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [Sidebar, Header],
+  imports: [Sidebar, Header, RouterOutlet, NgIf],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  isChildRouteActive = false;
   
+  constructor(private router: Router) {}
+
+  goToTransfer() {
+    this.isChildRouteActive = true;
+    this.router.navigate(['/components/transfer-details']);
+  }
+
+  onChildActivate() {
+    this.isChildRouteActive = true;
+  }
 }
