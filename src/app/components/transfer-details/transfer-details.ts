041e094497ffe753e251e6678bfa99a99d723da3
Hano Varghese <hanojoby@gmail.com>
2026-01-12 12:24:46 +0530

    payement buttons added

---
+++ src/app/components/transfer-details/transfer-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transfer-details',
+  imports: [],
+  templateUrl: './transfer-details.html',
+  styleUrl: './transfer-details.scss',
+})
+export class TransferDetails {
+
+}
